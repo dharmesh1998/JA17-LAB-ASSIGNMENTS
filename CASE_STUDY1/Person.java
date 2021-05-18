3a2f123cbacec3bb92b124a7643ea8d1ae11aab9
dharmesh1998 <64842067+dharmesh1998@users.noreply.github.com>
2021-05-18 17:40:56 +0530

    Add files via upload

---
+++ CASE_STUDY1/Person.java
@@ -0,0 +1,19 @@
+package Capgi;
+
+public class Person {
+    public String name;
+    public String address;
+
+
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+
+    public void setAddress(String address) { this.address = address; }
+    public void setName(String name) { this.name = name; }
+
+
+    public String toString() {
+        return "Person " + this.getName() + ", address= " + this.getAddress();
+    }
+
+}

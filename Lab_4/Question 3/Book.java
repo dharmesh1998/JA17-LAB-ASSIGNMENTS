0bd5d02125312fabc1302eba498922fdf167d3a4
dharmesh1998 <64842067+dharmesh1998@users.noreply.github.com>
2021-05-19 12:45:35 +0530

    Add files via upload

---
+++ Lab_4/Question 3/Book.java
@@ -0,0 +1,6 @@
+public class Book extends WrittenItem{
+	public Book(int identificationNumber, String title, int numberOfCopies, String author) {
+		super(identificationNumber,title,numberOfCopies,author);
+	}
+	
+}

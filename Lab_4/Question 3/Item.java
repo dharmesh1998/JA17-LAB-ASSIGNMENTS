43598069288a7b6d8ad40f4d522cb6c8441d47a1
dharmesh1998 <64842067+dharmesh1998@users.noreply.github.com>
2021-05-20 22:39:57 +0530

    Update Item.java

--- Lab_4/Question 3/Item.java
+++ Lab_4/Question 3/Item.java
@@ -3,53 +3,37 @@ public abstract class Item {
 	private int identificationNumber;
 	private String title;
 	private int numberOfCopies;
-	/**
-	 * @param identificationNumber
-	 * @param title
-	 * @param numberOfCopies
-	 */
+	
 	public Item(int identificationNumber, String title, int numberOfCopies) {
 		this.identificationNumber = identificationNumber;
 		this.title = title;
 		this.numberOfCopies = numberOfCopies;
 	}
-	/**
-	 * @return the identificationNumber
-	 */
+	
 	public int getIdentificationNumber() {
 		return identificationNumber;
 	}
-	/**
-	 * @param identificationNumber the identificationNumber to set
-	 */
+	
 	public void setIdentificationNumber(int identificationNumber) {
 		this.identificationNumber = identificationNumber;
 	}
-	/**
-	 * @return the title
-	 */
+	
 	public String getTitle() {
 		return title;
 	}
-	/**
-	 * @param title the title to set
-	 */
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	/**
-	 * @return the numberOfCopies
-	 */
+	
 	public int getNumberOfCopies() {
 		return numberOfCopies;
 	}
-	/**
-	 * @param numberOfCopies the numberOfCopies to set
-	 */
+	
 	public void setNumberOfCopies(int numberOfCopies) {
 		this.numberOfCopies = numberOfCopies;
 	}
-	@Override
+	
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
@@ -58,7 +42,7 @@ public abstract class Item {
 		result = prime * result + ((title == null) ? 0 : title.hashCode());
 		return result;
 	}
-	@Override
+	
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -78,7 +62,7 @@ public abstract class Item {
 			return false;
 		return true;
 	}
-	@Override
+	
 	public String toString() {
 		return "Item [identificationNumber=" + identificationNumber + ", title=" + title + ", numberOfCopies="
 				+ numberOfCopies + "]";

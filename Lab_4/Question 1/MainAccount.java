034260108cb722a31234fe470e62feb10fee5ad5
dharmesh1998 <64842067+dharmesh1998@users.noreply.github.com>
2021-05-20 22:37:42 +0530

    Update MainAccount.java

--- Lab_4/Question 1/MainAccount.java
+++ Lab_4/Question 1/MainAccount.java
@@ -2,28 +2,23 @@ public class MainAccount {
 
 	public static void main(String[] args) {
 		
-		Person p1=new Person("Kathy", 22.0f);
-		Person p2=new Person("Smith", 44.0f);
+		Person p1=new Person("DHARMESH", 100.0f);
+		Person p2=new Person("SONI", 68.0f);
 		
-		Account acc1=new Account((long)(Math.random()*Math.pow(10, 16)),3000,p1);
-		Account acc2=new Account((long)(Math.random()*Math.pow(10, 16)),2000,p2);
+		Account acc1=new Account((long)(Math.random()*Math.pow(10, 16)),1000,p1);
+		Account acc2=new Account((long)(Math.random()*Math.pow(10, 16)),3000,p2);
 		
 		System.out.println("Account No.\tAccount Holder\t\tIntial Balance");
-		System.out.println("==========================================================");
-		System.out.println(p1.name+"\t\t"+acc1.accNum+"\t"+acc1.balance);
-		System.out.println(p2.name+"\t\t"+acc2.accNum+"\t"+acc2.balance);
+		System.out.println(p1.name+"  "+acc1.accNum+"  "+acc1.balance);
+		System.out.println(p2.name+"  "+acc2.accNum+"  "+acc2.balance);
 		
 		
-		acc2.deposit(2000);
-		acc1.withdraw(1000);
-		
-		System.out.println();
-		System.out.println("Updated Account details are:");
-		System.out.println();
+		acc2.deposit(5000);
+		acc1.withdraw(7000);
+		System.out.println("Account details are:");
 		System.out.println("Account No.\tAccount Holder\t\tIntial Balance");
-		System.out.println("==========================================================");
-		System.out.println(p1.name+"\t\t"+acc1.accNum+"\t"+acc1.balance);
-		System.out.println(p2.name+"\t\t"+acc2.accNum+"\t"+acc2.balance);
+		System.out.println(p1.name+" "+acc1.accNum+" "+acc1.balance);
+		System.out.println(p2.name+" "+acc2.accNum+" "+acc2.balance);
 	}
 
 }

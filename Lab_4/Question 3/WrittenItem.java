cec233dc3afa8060891d17e2f0a552af8e13c25d
dharmesh1998 <64842067+dharmesh1998@users.noreply.github.com>
2021-05-20 22:41:28 +0530

    Update WrittenItem.java

--- Lab_4/Question 3/WrittenItem.java
+++ Lab_4/Question 3/WrittenItem.java
@@ -7,21 +7,17 @@ public abstract class WrittenItem extends Item {
 		this.author=author;
 	}
 
-	/**
-	 * @return the author
-	 */
+	
 	public String getAuthor() {
 		return author;
 	}
 
-	/**
-	 * @param author the author to set
-	 */
+	
 	public void setAuthor(String author) {
 		this.author = author;
 	}
 	
-	@Override
+
 	public void print() {
 		super.print();
 		System.out.println("Author : "+author);

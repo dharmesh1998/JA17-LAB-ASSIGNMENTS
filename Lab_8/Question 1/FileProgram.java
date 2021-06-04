d27da6e7e0520e7a710fb5ea037af5c445f16da5
dharmesh1998 <64842067+dharmesh1998@users.noreply.github.com>
2021-06-04 17:52:31 +0530

    Update FileProgram.java

--- Lab_8/Question 1/FileProgram.java
+++ Lab_8/Question 1/FileProgram.java
@@ -4,9 +4,9 @@ public class FileProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File sm_source=new File("D:\\CapgeminiJA17\\LabBook_Programs\\src\\com\\capgemini\\shrijit\\lab8\\source.txt"); // initializing Shrijit Majumder's source file
-		File sm_target=new File("D:\\CapgeminiJA17\\LabBook_Programs\\src\\com\\capgemini\\shrijit\\lab8\\target.txt"); // initializing Shrijit Majumder's target file
-		CopyDataThread cdt = new CopyDataThread(sm_source,sm_target);
+		File d1_source=new File("D:\\CapgeminiJA17\\LabBook_Programs\\src\\com\\capgemini\\source.txt");
+		File d2_target=new File("D:\\CapgeminiJA17\\LabBook_Programs\\src\\com\\capgemini\\target.txt");  
+		CopyDataThread cdt = new CopyDataThread(d1_source,d2_target);
 		cdt.start();
 	}
 

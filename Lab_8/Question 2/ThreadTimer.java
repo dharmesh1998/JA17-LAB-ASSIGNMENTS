cb10c626ece6c3865d86a50eeacb643ebde96682
dharmesh1998 <64842067+dharmesh1998@users.noreply.github.com>
2021-06-04 17:50:44 +0530

    Create ThreadTimer.java

---
+++ Lab_8/Question 2/ThreadTimer.java
@@ -0,0 +1,36 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class ThreadTimer implements Runnable{
+	
+	// synchronized display method to display the present time 
+	public synchronized void display() {
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		System.out.println(sdf.format(date));
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.print("Present time : ");
+		while(true) {
+			try {
+				display();
+				Thread.sleep(10000);
+				System.out.print("Time after 10 seconds : ");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThreadTimer threadtimer=new ThreadTimer();
+		Thread thread=new Thread(threadtimer);
+		thread.start();
+	}
+}

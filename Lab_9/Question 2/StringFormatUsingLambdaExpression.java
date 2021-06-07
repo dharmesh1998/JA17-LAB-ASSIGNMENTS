4eec6a204c8aa3f12a7c37b1982ef18572a98fc4
dharmesh1998 <64842067+dharmesh1998@users.noreply.github.com>
2021-06-07 10:46:09 +0530

    Create  StringFormatUsingLambdaExpression.java

---
+++ Lab_9/Question 2/StringFormatUsingLambdaExpression.java
@@ -0,0 +1,6 @@
+@FunctionalInterface
+public interface StringFormatUsingLambdaExpression {
+	
+	// method to insert a space between each characters of a given string
+	String returnExpression(String string);
+}

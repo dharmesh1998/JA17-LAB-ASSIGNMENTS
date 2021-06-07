99466d8e53786e87aff861fe01da55dcbe5ca9c5
dharmesh1998 <64842067+dharmesh1998@users.noreply.github.com>
2021-06-07 10:47:48 +0530

    Create UsernamePasswordValidationUsingLambdaExpression.java

---
+++ Lab_9/Question 3/UsernamePasswordValidationUsingLambdaExpression.java
@@ -0,0 +1,6 @@
+@FunctionalInterface
+public interface UsernamePasswordValidationUsingLambdaExpression {
+
+	//boolean method to validate username & password
+	boolean validateusername_password(String username,String password);
+}

8f3aae6f19a10f85636ab513e006ac6d07b5233f
dharmesh1998 <64842067+dharmesh1998@users.noreply.github.com>
2021-06-07 10:45:31 +0530

    Create StringFormatMain.java

---
+++ Lab_9/Question 2/StringFormatMain.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class StringFormatMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sm=new Scanner(System.in); 
+		StringFormatUsingLambdaExpression se = (string)->{
+			char characters[]=string.toCharArray();
+			String result="";
+			for(int i=0;i<characters.length-1;i++) {
+				result+=characters[i]+" ";
+			}
+			return result+characters[characters.length-1];
+		};
+		System.out.print("Enter the string : ");
+		String string=sm.next();
+		System.out.println("\nResultant string = "+se.returnExpression(string));
+		
+		sm.close();
+
+	}
+
+}
